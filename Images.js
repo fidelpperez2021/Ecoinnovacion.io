cea0d0b2c49f4e0baca31642144f364e2e9db530
Fidel Ernesto Perez Bedoya <92316606+fidelpperez2021@users.noreply.github.com>
2024-06-27 02:10:37 -0500

    Update Images.js

--- Images.js
+++ Images.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $('#imageModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget);
     var imageSrc = button.data('src');
@@ -6,13 +5,4 @@ $('#imageModal').on('show.bs.modal', function (event) {
     var modal = $(this);
     modal.find('.modal-body img').attr('src', imageSrc);
     modal.find('#imageTitle').text(imageTitle);
-=======
-$('#imageModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var imageSrc = button.data('src');
-    var imageTitle = button.data('title');
-    var modal = $(this);
-    modal.find('.modal-body img').attr('src', imageSrc);
-    modal.find('#imageTitle').text(imageTitle);
->>>>>>> 86dde0c8a69128f81c6821935231cd99efc1ccda
 });
