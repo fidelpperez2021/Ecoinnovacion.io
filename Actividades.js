f89a8034bdfc4c18cfa71dde03798486eec2bdeb
Fidel Ernesto Perez Bedoya <92316606+fidelpperez2021@users.noreply.github.com>
2024-06-27 02:16:40 -0500

    Update Actividades.js

--- Actividades.js
+++ Actividades.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {s
     const activities = [
         { activity: 'Difusión de nuestro proyecto de limpieza y reforestación del manglar.', date: '08-15/07/2024', remark: 'INICIO' },
         { activity: 'Capacitaciones técnicas para la comunidad', date: '05-23/08/2024' },
